93d528b470b4a0f69ef4a480e9361d079090d735
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-04-26 17:54:55 +0530

    ch

---
+++ segment-notes.js
@@ -0,0 +1,57 @@
+// ok take a look at this:
+
+const response = {
+  originalTimestamp: "2025-04-24T13:26:08.062Z",
+  properties: {
+    path: "/track",
+    referrer:
+      "https://vqgwmvp1run-yy3c--3000--d4eba4a9.local-credentialless.webcontainer.io/.localservice@service.worker.d4eba4a9.js",
+    search: "",
+    title: "Track Events",
+    url: "https://vqgwmvp1run-yy3c--3000--d4eba4a9.local-credentialless.webcontainer.io/track",
+  },
+  userId: null,
+  context: {
+    page: {
+      path: "/track",
+      referrer:
+        "https://vqgwmvp1run-yy3c--3000--d4eba4a9.local-credentialless.webcontainer.io/.localservice@service.worker.d4eba4a9.js",
+      search: "",
+      title: "Track Events",
+      url: "https://vqgwmvp1run-yy3c--3000--d4eba4a9.local-credentialless.webcontainer.io/track",
+    },
+    userAgentData: {
+      brands: [
+        {
+          brand: "Google Chrome",
+          version: "135",
+        },
+        {
+          brand: "Not-A.Brand",
+          version: "8",
+        },
+        {
+          brand: "Chromium",
+          version: "135",
+        },
+      ],
+      mobile: false,
+      platform: "Windows",
+    },
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+    locale: "en-US",
+    library: {
+      name: "analytics.js",
+      version: "npm:next-1.53.0",
+    },
+    ip: "49.43.6.245",
+  },
+  integrations: {},
+  messageId: "ajs-next-97cd6df479c5dda6941c9813edcd1b0d", // IMP
+  anonymousId: "a5fba577-5294-4810-a12d-a22737315d4a", // IMP
+  receivedAt: "2025-04-24T13:26:09.262Z",
+  sentAt: "2025-04-24T13:26:08.090Z",
+  timestamp: "2025-04-24T13:26:09.234Z", // IMP
+  type: "page", // IMP
+};
